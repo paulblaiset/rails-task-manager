379d0017cd6fd504295e17c985363841de3fb803
Paul Henri Blaiset <paul.blaiset@gmail.com>
2018-05-15 17:15:14 +0200

    success tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get "/tasks", to: "tasks#index"
+
+get "/tasks/new", to: "tasks#new"
+post "tasks",  to:"tasks#create"
+
+get "/tasks/:id", to: "tasks#show", as:"task"
+
+get "tasks/:id/edit", to:"tasks#edit", as:"edit"
+patch "tasks/:id/", to:"tasks#update"
+
+delete "tasks/:id", to: "tasks#destroy", as:"delete"
+
 end
+
+# # As a user, I can edit a task (mark as completed / update title & details)
+
+# # As a user, I can remove a task
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
